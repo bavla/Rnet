6db68888b458af919c3a29953d9bfe8bdcdfe88f
bavla <vladimir.batagelj@gmail.com>
2019-11-23 23:21:45 +0100

    Add files via upload

--- R/Pajek.R
+++ R/Pajek.R
@@ -1,10 +1,11 @@
-%
-% Pajek.R
-% reading and saving of Pajek data in R (matrices and vectors)
-%
-% by Vladimir Batagelj, November 2019
-%
-
+#
+# Pajek.R
+# reading and saving of Pajek data in R (matrices and vectors)
+#
+# by Vladimir Batagelj, November 2019
+#
+# https://github.com/bavla/cluRC/blob/master/readPajekNet.R
+#
 
 clu2vector <- function(f,skip=1){
   read.table(f,skip=skip,colClasses=c("integer"),header=FALSE)$V1
@@ -14,7 +15,23 @@ vec2vector <- function(f,skip=1){
   read.table(f,skip=skip,colClasses=c("numeric"),header=FALSE)$V1
 }
 
-mat2Pajek <- function(M,Net="Pajek.net"){
+net2matrix <- function(f,skip=0){
+# reads a network from Pajek's net file; skip initial comments lines
+   L <- readLines(f)
+   st <- grep("\\*",L)
+   S <- unlist(strsplit(L[st[1]]," "))
+   n <- as.integer(S[2]); n1 <- st[1]+1; n2 <- st[2]-1
+   m1 <- st[2]+1; m2 <- length(L); m <- m2-m1+1
+   Names <- unlist(strsplit(L[n1:n2],'"'))[2*(1:n)]
+   R <- matrix(data=0,nrow=n,ncol=n,dimnames=list(Names,Names))
+   S <- unlist(strsplit(L[m1:m2],'[[:space:]]+'))
+   b <- as.integer(S[3*(1:m)-2]); e <- as.integer(S[3*(1:m)-1]); v <- as.numeric(S[3*(1:m)])
+   for(k in 1:m) R[b[k],e[k]] <- R[b[k],e[k]]+v[k]
+#   rownames(R) <- Names
+   return(R)
+}
+
+matrix2net <- function(M,Net="Pajek.net"){
   n <- nrow(M); net <- file(Net,"w")
   cat("% mat2Pajek",date(),"\n*vertices",n,"\n",file=net)
   RN <- row.names(M)
@@ -24,7 +41,7 @@ mat2Pajek <- function(M,Net="Pajek.net"){
   close(net)
 }
 
-bip2Pajek <- function(M,Net="Pajek.net"){
+bimatrix2net <- function(M,Net="Pajek.net"){
   n <- nrow(M); m <- ncol(M); net <- file(Net,"w")
   cat("% bip2Pajek",date(),"\n*vertices",n+m,n,"\n",file=net)
   RN <- dimnames(M)[[1]]; CN <- dimnames(M)[[2]];
@@ -35,14 +52,14 @@ bip2Pajek <- function(M,Net="Pajek.net"){
   close(net)
 }
 
-clu2Pajek <- function(C,Clu="Pajek.clu"){
+vector2clu <- function(C,Clu="Pajek.clu"){
   n <- length(C); clu <- file(Clu,"w")
   cat("% clu2Pajek",date(),"\n*vertices",n,"\n",file=clu)
   cat(C,sep="\n",file=clu)
   close(clu)
 }
 
-vec2Pajek <- function(X,Vec="Pajek.vec"){
+vector2vec <- function(X,Vec="Pajek.vec"){
   n <- length(X); vec <- file(Vec,"w")
   cat("% vec2Pajek",date(),"\n*vertices",n,"\n",file=vec)
   cat(X,sep="\n",file=vec)
