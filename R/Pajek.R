178815445a9828110c7fa2c1516af660c55f1b1b
bavla <vladimir.batagelj@gmail.com>
2019-12-08 05:37:06 +0100

    Add files via upload

--- R/Pajek.R
+++ R/Pajek.R
@@ -52,6 +52,21 @@ bimatrix2net <- function(M,Net="Pajek.net"){
   close(net)
 }
 
+uv2net <- function(u,v,w=NULL,Net="Pajek.net",twomode=FALSE){
+  net <- file(Net,"w")
+  if(is.null(w)) w <- rep(1,length(u))
+  RN <- levels(u); n <- length(RN)
+  if(twomode) {CN <- levels(v);  m <- length(CN)}
+  U <- as.integer(u); V <- as.integer(v)
+  if(twomode) cat("% uv2Pajek",date(),"\n*vertices",n+m,n,"\n",file=net) else
+    cat("% uv2Pajek",date(),"\n*vertices",n,"\n",file=net)
+  for(i in 1:n) cat(i,' "',RN[i],'"\n',sep="",file=net)
+  if(twomode) for(i in 1:m) cat(i+n,' "',CN[i],'"\n',sep="",file=net)
+  cat("*arcs\n",file=net)
+  for(i in 1:length(u)) cat(U[i],V[i]+twomode*n,w[i],"\n",file=net)
+  close(net)
+}
+
 vector2clu <- function(C,Clu="Pajek.clu"){
   n <- length(C); clu <- file(Clu,"w")
   cat("% clu2Pajek",date(),"\n*vertices",n,"\n",file=clu)
